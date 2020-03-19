feec87606dc6bfbec51236e68a4c05ae6654f310
Angela Yu <angelayu@Angelas-MacBook-Pro-10.local>
2020-03-19 16:58:04 -0400

    started making analytics page

--- soundhub/src/js/Analytics.js
+++ soundhub/src/js/Analytics.js
@@ -12,7 +12,36 @@ let Analytics = (props) =>
         <div className="Analytics">
             <AppHeader pageTitle="ANALYTICS"/>
             <AppFooter/>
-
+            <div className="analyticsBody">
+            <div className="category">
+              <p className="title">This month you listened to...</p>
+              <p className="minutes">10,123</p>
+              <p className="center">minutes of music</p>
+              <div className="otherMetrics">
+                <div className="metric">
+                  <p className="metricText">17</p>
+                  <p className="center">albums</p>
+                </div>
+                <div className="metric">
+                  <p className="metricText">200</p>
+                  <p className="center">artists</p>
+                </div>
+                <div className="metric">
+                  <p className="metricText">250</p>
+                  <p className="center">songs</p>
+                </div>
+              </div>
+            </div>
+            <div className="category">
+              <p className="title">Favorite Genres This Month</p>
+            </div>
+            <div className="category">
+              <p className="title">Top Song of the Month</p>
+            </div>
+            <div className="category">
+              <p className="title">Average Track Mood This Month</p>
+            </div>
+            </div>
         </div>
     )
 }
