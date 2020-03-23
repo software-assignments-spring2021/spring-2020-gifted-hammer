ada2fcf86e2d55d4914e20800f1e39ad5f3ab09b
Quin Scacheri <qs366@nyu.edu>
2020-03-23 17:52:13 -0400

    added temporary logo

--- frontend/src/js/AppHeader.jsx
+++ frontend/src/js/AppHeader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../css/AppHeader.css';
 import spotify from '../spotify_logo.png';
-import logo from '../assets/logo512.png';
+import logo from '../assets/logo.svg';
 
 function AppHeader(props) {
     return (
@@ -13,13 +13,12 @@ function AppHeader(props) {
                 </div>
                 <div className="buttonContainer">
                     <a className="spotify_container" href="https://www.spotify.com/us/">
-                        <img alt="Spotify_logo" src = {spotify}></img>
+                        <img alt="Spotify_logo" src={spotify}></img>
                         <span className="spotifyButton">Connect to Spotify</span>
                     </a>
                 </div>
                 <h1 className='pageTitle'>{props.pageTitle}</h1>
             </div>
-            {/* <hr className="divider"/> */}
         </>
     )
 }
