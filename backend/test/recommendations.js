2c82a44bd5a671f0b970faf7dd2f03cfa02aeb02
Quin Scacheri <qs366@nyu.edu>
2020-04-13 12:17:00 -0400

    added tests for token getting and seed recs

---
+++ backend/test/recommendations.js
@@ -0,0 +1,62 @@
+var expect = require('chai').expect;
+const axios = require('axios').default;
+var chai = require('chai'), chaiHttp = require('chai-http');
+var expect = chai.expect;
+const app = require("../app"); 
+
+chai.use(chaiHttp);
+
+let userToken = '';
+
+describe('Discovery', function () {
+    let userToken = null;
+    it('Get Basic Token', function (done) { // <= Pass in done callback
+        chai.request(app)
+            .get('/token')
+            .end(function (err, res) {
+                userToken = res.body.token
+                expect(userToken).to.not.be.empty;
+                done();                               // <= Call done to signal callback end
+            });
+    });
+
+    it('Get Recs (Seed: Tool)', function (done) { // <= Pass in done callback
+        const data = {
+            '_method': 'post',
+            token: userToken,
+            artist: 'tool',
+            tempo: .5,
+            popularity: .5,
+            energy: .5,
+            mood: .5,
+            vocals: .5,
+            danceability: .5
+        }
+
+        chai.request('http://localhost:3001')
+            .post('/search')
+            .send(data)
+            .end(function (err, res) {
+                console.log(res.body[0]);
+                expect(res.body[0]).to.have.all.keys(
+                    "album",
+                    "artists",
+                    "available_markets",
+                    "disc_number",
+                    "duration_ms",
+                    "explicit",
+                    "external_ids",
+                    "external_urls",
+                    "href",
+                    "id",
+                    "is_local",
+                    "name",
+                    "popularity",
+                    "preview_url",
+                    "track_number",
+                    "type",
+                    "uri")
+                done();
+            });
+    });
+})
