b535aba1d651d5aa6eacdec7f5c8afb1f61b301f
Wu-Bowen <ericwu692@yahoo.com>
2020-04-14 14:32:59 -0400

    Added token and genre breakdown

--- frontend/src/js/Analytics.jsx
+++ frontend/src/js/Analytics.jsx
@@ -8,16 +8,19 @@ let Analytics = (props) => {
 
     const [topSongData, setTopSongData] = useState({})
     const [topGenres, setTopGenres] = useState({})
+    const [genreBreakdown, setGenreBreakdown] = useState([])
+    const [trackMoods, setTrackMoods] = useState([])
+    const accessToken = props.accessToken
 
     const [hasError, setErrors] = useState(false);
     async function fetchData() {
-        console.log(props.token);
+        console.log(accessToken);
 
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-                "token": "BQCD-biLKXEOxjeRr5suOq1Sz-54wloFrfxu4H3VZseB4OEbdmP1oQoe4lNKZsQtQQUQ4IBjHDNAgH3IZbzxmKwplUL3bEPx06x6tr_BGYmB7H3fLl2LUCvnD86P2o_ssqitDFr-6mqwfD2sXxr2zKYrnwiupKLUtTX_vgHIQuZdbzTz_E906Hwn4E5pls2NNqs-VzpxX7C5m1s3hVFdrQ0MmZtAJkC-Stvf65DUTGZS8M-8IqioggZnJVXAgh9y7aUnTzMHFvk"
+                "token": accessToken
             })
         };
 
@@ -29,10 +32,18 @@ let Analytics = (props) => {
         fetch('/topGenres', requestOptions)
         .then(response => response.json())
         .then(data => formatMonthlyGenre(data));
+
+        fetch('/genreBreakdown', requestOptions)
+        .then(response => response.json())
+        .then(data => setGenreBreakdown(data));
+
+        fetch('/trackMoods', requestOptions)
+        .then(response => response.json())
+        .then(data => setTrackMoods(data));
         }
 
+
         const formatTopSong = data => {
-            console.log(data);
             let topSong = {};
             topSong.name = data[0].name;
             topSong.artist = data[0].artists[0].name;
@@ -42,20 +53,15 @@ let Analytics = (props) => {
 
         
         const formatMonthlyGenre = data => {
-            console.log(data);
             let topGenre = {};
             topGenre.firstName = data[0].genre;
-            topGenre.firstCount = data[0].count;
             topGenre.firstImage = data[0].image[0];
             topGenre.secondName = data[1].genre;
-            topGenre.secondCount = data[1].count;
             topGenre.secondImage = data[1].image[1];
             topGenre.thirdName = data[2].genre;
-            topGenre.thirdCount = data[2].count;
             topGenre.thirdImage = data[2].image[2];
             setTopGenres(topGenre);
         }
-        
 
     useEffect(() => {
         fetchData();
@@ -94,15 +100,15 @@ let Analytics = (props) => {
                 </div>
             </div>
             <div className='section'>
-                <SectionHeading name='Average track mood'></SectionHeading>
+                <SectionHeading name='Mood of Your Songs'></SectionHeading>
                 <div className='moodChart'>
-                    <MoodChart></MoodChart>
+                    <MoodChart data={trackMoods}></MoodChart>
                 </div>
             </div>
             <div className='section'>
                 <SectionHeading name='Your genere breakdown'></SectionHeading>
                 <div className='sectionBody genereChart'>
-                    <GenereChart></GenereChart>
+                    <GenereChart data={genreBreakdown}></GenereChart>
                 </div>
             </div>
         </div>
@@ -141,87 +147,19 @@ let Genere = (props) => {
     )
 }
 let MoodChart = (props) => {
-    const data = [
-        {
-            name: 'song1', avgMoodScore: 25,
-        },
-        {
-            name: 'song2', avgMoodScore: 91,
-        },
-        {
-            name: 'song', avgMoodScore: 70,
-        },
-        {
-            name: 'song4', avgMoodScore: 27,
-        },
-        {
-            name: 'song5', avgMoodScore: 33,
-        },
-        {
-            name: 'song6', avgMoodScore: 65,
-        },
-        {
-            name: 'song7', avgMoodScore: 91,
-        },
-        {
-            name: 'song8', avgMoodScore: 92,
-        },
-        {
-            name: 'song9', avgMoodScore: 73,
-        },
-        {
-            name: 'song10', avgMoodScore: 24,
-        },
-        {
-            name: 'song11', avgMoodScore: 31,
-        },
-        {
-            name: 'song12', avgMoodScore: 25,
-        },
-        {
-            name: 'song13', avgMoodScore: 30,
-        },
-        {
-            name: 'song14', avgMoodScore: 41,
-        },
-        {
-            name: 'song15', avgMoodScore: 21,
-        },
-        {
-            name: 'song16', avgMoodScore: 47,
-        },
-        {
-            name: 'song17', avgMoodScore: 12,
-        },
-        {
-            name: 'song18', avgMoodScore: 15,
-        },
-        {
-            name: 'song18', avgMoodScore: 90,
-        },
-        {
-            name: 'song20', avgMoodScore: 81,
-        },
-        {
-            name: 'song21', avgMoodScore: 77,
-        },
-        {
-            name: 'song22', avgMoodScore: 27,
-        },
-        {
-            name: 'song23', avgMoodScore: 37,
-        },
-        {
-            name: 'song24', avgMoodScore: 35,
-        },
-        {
-            name: 'song25', avgMoodScore: 60,
-        }
-    ];
+    const data= [];
+    console.log(props);
+    for (let i = 0; i< props["data"].length; i++){
+        const dataval = {};
+        dataval.moodScore = props["data"][i]["valence"];
+        dataval.name = props["data"][i]["id"];
+        data.push(dataval);
+    }
+    console.log(data);
 
     return (
         <BarChart
-            width={700}
+            width={1200}
             height={200}
             data={data}
             margin={{
@@ -231,15 +169,24 @@ let MoodChart = (props) => {
         <XAxis dataKey="name" tick={false}/>
         <YAxis />
         <Tooltip/>
-        <Bar dataKey="avgMoodScore" fill="#8884d8" />
+        <Bar dataKey="moodScore" fill="#8884d8" />
         </BarChart>
     )
 }
 let GenereChart = (props) => {
-    const data01 = [
-        { name: 'Rock', value: 25 }, { name: 'Metal', value: 30 },
-        { name: 'Pop', value: 35 }, { name: 'Lo-Fi', value: 10 }
-    ];
+    // const data01 = [
+    //     { name: 'Rock', value: 25 }, 
+    //     { name: 'Metal', value: 30 },
+    //     { name: 'Pop', value: 35 }, 
+    //     { name: 'Lo-Fi', value: 10 }
+    // ];
+    const data01 = [];
+    for (let i = 0; i< props["data"].length; i++){
+        const dataval = {};
+        dataval.name = props["data"][i].name;
+        dataval.value = props["data"][i].value;
+        data01.push(dataval);
+    }
 
 
     return (
@@ -247,7 +194,7 @@ let GenereChart = (props) => {
             margin={{
                 top: 0, right: 40, left: 20, bottom: 0,
             }}>
-            <Pie dataKey='value' data={data01} cx={400} cy={200} innerRadius={40} outerRadius={80} fill="#8884d8" />
+            <Pie dataKey='value' data={data01} cx={400} cy={200} innerRadius={80} outerRadius={160} fill="#8884d8" />
             <Tooltip />
         </PieChart>
     )
--- frontend/src/js/config.js
+++ frontend/src/js/config.js
@@ -4,6 +4,7 @@ const redirectUri = "http://localhost:3000";
 const scopes = [
   "user-read-currently-playing",
   "user-read-playback-state",
+  "user-top-read",
   "streaming", 
   "user-read-email", 
   "user-read-private"
