9762ccbc17c4db032ab2f5d682490cdf586c6ac8
Wu-Bowen <ericwu692@yahoo.com>
2020-04-25 22:58:27 -0400

    moodGoose schema

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,13 +3,27 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true)
+
+// mongoose trackSchema
 const TracksSchema = new mongoose.Schema({
     locationCode: { type: String, required: true },
     events: { type: Array, required: true },
 });
 mongoose.model("Tracks", TracksSchema)
 const Tracks = mongoose.model("Tracks")
-mongoose.connect('mongodb+srv://ryan:Magicpotato2@cluster0-wjvnq.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://giftedHammer:giftedHammer@cluster0-smurl.mongodb.net/spotilytics?retryWrites=true&w=majority', { useNewUrlParser: true });
+
+
+// mongoose moodSchema
+const moodsSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
+    moods: { type: Array, required: true },
+});
+mongoose.model("Moods", moodsSchema)
+const Moods = mongoose.model("Moods")
+mongoose.connect('mongodb+srv://giftedHammer:giftedHammer@cluster0-smurl.mongodb.net/spotilytics?retryWrites=true&w=majority', { useNewUrlParser: true });
+
+
 //how u create a entry
 /*
 const tracks = new Tracks({locationCode:"0000",data:{test:"blah!",testo:["spoo","bah"]})
